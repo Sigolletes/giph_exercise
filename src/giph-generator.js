5d23a6e4b3b0e108d6f57e2ec838229361a439cb
Sigolletes <juditreverter87@gmail.com>
2023-01-25 12:51:43 +0100

    Add search giph function

--- src/giph-generator.js
+++ src/giph-generator.js
@@ -1,5 +1,6 @@
 const giphGenerator = (function() {
   const $main = document.querySelector("#main");
+  const $search = document.querySelector("#search");
 
   const api_key = 'GEuWWpT4pvXl1MSecs0yVBsmefvFfWdl';
 
@@ -7,8 +8,10 @@ const giphGenerator = (function() {
     const giphURL = `https://api.giphy.com/v1/gifs/trending?api_key=${api_key}`;
     const response = await fetch(giphURL, {mode: 'cors'});
     const giphData = await response.json();
-    console.log(giphData);
 
+    $main.innerHTML = "";
+    $main.classList.remove("flex");
+    $main.classList.add("grid");
     for (let i = 0; i < 4; i++) {
       let $img = document.createElement('img');
       $main.appendChild($img);
@@ -16,7 +19,28 @@ const giphGenerator = (function() {
     }
   }
 
-  
+  async function searchGiph() {
+    const giphURL = `https://api.giphy.com/v1/gifs/translate?api_key=${api_key}&s=${$search.value}`;
+    const response = await fetch(giphURL, {mode: 'cors'});
+    const giphData = await response.json();
+    console.log(giphData);
+
+    $main.innerHTML = "";
+    $main.classList.remove("grid");
+    $main.classList.add("flex");
+    let $img = document.createElement('img');
+    $main.appendChild($img);
+    $img.src = giphData.data.images.original.url;
+  }
+
+  $search.addEventListener('input', (e) => {
+    e.preventDefault();
+    if ($search.value) {
+      searchGiph();
+    } else {
+      trending();
+    }
+  });
 
   return {
     trending
--- src/index.js
+++ src/index.js
@@ -1,4 +1,4 @@
 import style from "./style.css";
 import giphGenerator from "./giph-generator";
 
-giphGenerator.giph();
+giphGenerator.trending();
