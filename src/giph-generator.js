bcfb170975f694bd57a9853db10062e6b5a45c61
Sigolletes <juditreverter87@gmail.com>
2023-01-25 16:06:12 +0100

    Add more cool functions and end project

--- src/giph-generator.js
+++ src/giph-generator.js
@@ -1,6 +1,8 @@
 const giphGenerator = (function() {
-  const $main = document.querySelector("#main");
-  const $search = document.querySelector("#search");
+  const $main = document.querySelector('#main');
+  const $search = document.querySelector('#search');
+  const $trending = document.querySelector('#trending');
+  const $random = document.querySelector('#random');
 
   const api_key = 'GEuWWpT4pvXl1MSecs0yVBsmefvFfWdl';
 
@@ -9,9 +11,9 @@ const giphGenerator = (function() {
     const response = await fetch(giphURL, {mode: 'cors'});
     const giphData = await response.json();
 
-    $main.innerHTML = "";
-    $main.classList.remove("flex");
-    $main.classList.add("grid");
+    $main.innerHTML = '';
+    $main.classList.remove('flex');
+    $main.classList.add('grid');
     for (let i = 0; i < 4; i++) {
       let $img = document.createElement('img');
       $main.appendChild($img);
@@ -19,18 +21,54 @@ const giphGenerator = (function() {
     }
   }
 
-  async function searchGiph() {
-    const giphURL = `https://api.giphy.com/v1/gifs/translate?api_key=${api_key}&s=${$search.value}`;
+  async function random() {
+    const giphURL = `https://api.giphy.com/v1/gifs/random?api_key=${api_key}`;
     const response = await fetch(giphURL, {mode: 'cors'});
     const giphData = await response.json();
-    console.log(giphData);
+    return giphData;
+  }
 
-    $main.innerHTML = "";
-    $main.classList.remove("grid");
-    $main.classList.add("flex");
-    let $img = document.createElement('img');
-    $main.appendChild($img);
-    $img.src = giphData.data.images.original.url;
+  $random.addEventListener('click', () => {
+    $main.innerHTML = '';
+    $main.classList.remove('flex');
+    $main.classList.add('grid');
+    for (let i = 0; i < 4; i++) {
+      let $img = document.createElement('img');
+      $main.appendChild($img);
+      random().then((giphData) => {
+        $img.src = giphData.data.images.original.url;
+      })
+    }
+  });
+
+  $trending.addEventListener('click', trending());
+
+  async function searchGiph() {
+    try {
+      const giphURL = `https://api.giphy.com/v1/gifs/translate?api_key=${api_key}&s=${$search.value}`;
+      const response = await fetch(giphURL, {mode: 'cors'});
+      const giphData = await response.json();
+  
+      $main.innerHTML = '';
+      $main.classList.remove('grid');
+      $main.classList.add('flex');
+      let $img = document.createElement('img');
+      $main.appendChild($img);
+      $img.classList.add('bigImg');
+      $img.src = giphData.data.images.original.url;
+    } catch (error) {
+      $main.innerHTML = '';
+      $main.classList.remove('grid');
+      $main.classList.add('flex');
+      let $h1 = document.createElement('h1');
+      $main.appendChild($h1);
+      $h1.innerText = 'No giphs with this name';
+      setTimeout(() => {
+        $main.innerHTML = '';
+        $search.value = '';
+        trending();
+      }, 5000);
+    }
   }
 
   $search.addEventListener('input', (e) => {
